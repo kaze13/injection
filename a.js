163107c0e0dd0bee08ebe34a381719c81ce2b49d
Minli Chen <cml_hawke0@163.com>
2015-10-13 10:13:41 +0800

    fix

--- a.js
+++ a.js
@@ -1,13 +1,13 @@
 R=0; x1=.1; y1=.05; x2=.25; y2=.24; x3=1.6; y3=.24; x4=300; y4=200; x5=300; y5=200; DI=parent.document.getElementsByClassName('menu_head'); DIL=DI.length; function A(){for(i=0; i-DIL; i++){DIS=DI[ i ].style; DIS.position='absolute';DIS.zIndex=1000; DIS.left=(Math.sin(R*x1+i*x2+x3)*x4+x5)+'px'; DIS.top=(Math.cos(R*y1+i*y2+y3)*y4+y5)+'px'}R++}setInterval('A()',5); void(0);
-var canvas = document.createElement('canvas');
+var canvas = parent.document.createElement('canvas');
 canvas.height=300;
 canvas.width=272;
 canvas.id='mario';
-document.body.appendChild(canvas);
-document.body.style.backgroundColor='black';
+parent.document.body.appendChild(canvas);
+parent.document.body.style.backgroundColor='black';
 
 
-var c = document.getElementById("mario");
+var c = parent.document.getElementById("mario");
 var ctx = c.getContext("2d");
 
 var marioArray=[
