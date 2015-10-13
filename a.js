c73707867b0f98d09221302210af19b450973a03
Minli Chen <cml_hawke0@163.com>
2015-10-13 10:11:05 +0800

    fix

--- a.js
+++ a.js
@@ -1,7 +1,7 @@
 R=0; x1=.1; y1=.05; x2=.25; y2=.24; x3=1.6; y3=.24; x4=300; y4=200; x5=300; y5=200; DI=parent.document.getElementsByClassName('menu_head'); DIL=DI.length; function A(){for(i=0; i-DIL; i++){DIS=DI[ i ].style; DIS.position='absolute';DIS.zIndex=1000; DIS.left=(Math.sin(R*x1+i*x2+x3)*x4+x5)+'px'; DIS.top=(Math.cos(R*y1+i*y2+y3)*y4+y5)+'px'}R++}setInterval('A()',5); void(0);
 var canvas = document.createElement('canvas');
-canvas.height=600;
-canvas.width=544;
+canvas.height=300;
+canvas.width=272;
 canvas.id='mario';
 document.body.appendChild(canvas);
 document.body.style.backgroundColor='black';
@@ -64,10 +64,10 @@ for(var i=0; i < marioArray.length; i++){
      
    }
   //move over 32px   
-  xPos+=32 
+  xPos+=16 
   }//end internal for loop
   //once ctx reaches end on canvas reset xPos to 0
   xPos=0;
   //move down 32px
-  yPos+=32;
+  yPos+=16;
 }//end for loop
